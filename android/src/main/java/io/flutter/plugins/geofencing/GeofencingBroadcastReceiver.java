b712ef65d03a9ae441ee57c46725457b414c91d3
BenjaminChevoor <bchevoor@gmail.com>
2019-01-01 10:54:36 -0500

    Starting java port

---
+++ android/src/main/java/io/flutter/plugins/geofencing/GeofencingBroadcastReceiver.java
@@ -0,0 +1,34 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugins.geofencing
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import io.flutter.view.FlutterMain;
+
+class GeofencingBroadcastReceiver extends BroadcastReceiver {
+
+    public static final String TAG = "GeofencingBroadcastReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        FlutterMain.ensureInitializationComplete(context, null);
+        GeofencingService.enqueueWork(context, intent);
+    }
+
+}
+
+
+//class GeofencingBroadcastReceiver : BroadcastReceiver() {
+//    companion object {
+//        private const val TAG = "GeofencingBroadcastReceiver"
+//    }
+//    override fun onReceive(context: Context, intent: Intent) {
+//        FlutterMain.ensureInitializationComplete(context, null)
+//        GeofencingService.enqueueWork(context, intent)
+//    }
+//}
